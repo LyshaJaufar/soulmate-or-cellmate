df34227b533ed86eb7ca308e9fd1ebff706bb075
LyshaJaufar <lysha.ahmd@gmail.com>
2022-07-16 23:59:18 +0500

    upd

--- src/components/FetchFelonData.js
+++ src/components/FetchFelonData.js
@@ -72,19 +72,19 @@ export default class FetchFelonData extends React.Component {
                 felonsJsx.push({
                     felonUrl: felon.url,
                     image: `${felon.url}/@@images/image/preview`,
-                    name: felon.aliases ? felon.aliases[0] : "missing",
-                    DOB: felon.dates_of_birth_used ? felon.dates_of_birth_used : "missing",
-                    nationality: felon.nationality ? felon.nationality : "missing",
-                    race: felon.race ? felon.race : "missing",
-                    placeOfBirth: felon.place_of_birth ? felon.place_of_birth : "missing",
-                    occupation: felon.occupation ? felon.occupation : "missing",
-                    hair: felon.hair ? felon.hair : "missing",
-                    eyes: felon.eyes ? felon.eyes : "missing",
-                    height: felon.height_max ? felon.height_max : "missing",
-                    weight: felon.weight_max ? felon.weight_max : "missing",
-                    redFlag: felon.warning_message ? felon.warning_message : "missing",
-                    description: felon.description ? felon.description : "missing"}
-                );
+                    name: felon.aliases ? felon.aliases[0] : null,
+                    DOB: felon.dates_of_birth_used ? felon.dates_of_birth_used[0] : null,
+                    nationality: felon.nationality ? felon.nationality : null,
+                    race: felon.race ? felon.race : null,
+                    placeOfBirth: felon.place_of_birth ? felon.place_of_birth : null,
+                    occupation: felon.occupation ? felon.occupation : null,
+                    hair: felon.hair ? felon.hair : null,
+                    eyes: felon.eyes ? felon.eyes : null,
+                    height: felon.height_max ? felon.height_max : null,
+                    weight: felon.weight_max ? felon.weight_max : null,
+                    redFlag: felon.warning_message ? felon.warning_message : null,
+                    description: felon.description ? felon.description : null
+                });
             });
         }
 
@@ -106,18 +106,69 @@ export default class FetchFelonData extends React.Component {
                 index: 1
             });
         }
-        if (felonsJsx[this.state.index].name !== "missing") {
+        console.log(this.state.felonsPages[0])
+        var heightFeet = null
+        var heightInches = null
+        var heightText = null
+        
+        if (felonsJsx[this.state.index].height != null) {
+            var height = parseInt(felonsJsx[this.state.index].height)/12
+            heightFeet = String(height)[0]
+            heightInches = String(height)[2]
+            heightText = heightFeet + "'" + heightInches
+        }
+
+        if (felonsJsx[this.state.index].redFlag != null){
+            var redFlagLower = (felonsJsx[this.state.index].redFlag).toLowerCase()
+        }
+        
+        var ethnicityImg = null
+        var personImg = null
+        var occupationImg = null
+        var locationImg = null
+        var redFlagImg = null
+        if (felonsJsx[this.state.index].nationality != null || !felonsJsx[this.state.index].race != null) {
+            ethnicityImg = <img src={ethnicity} class="icon"  alt="ethnicity" />
+        }
+        if (felonsJsx[this.state.index].height != null || !felonsJsx[this.state.index].weight != null) {
+            personImg = <img src={person} class="icon"  alt="person" />   
+        }
+        if (felonsJsx[this.state.index].occupation != null) {
+            occupationImg = <img src={occupation} class="icon"  alt="occupation" /> 
+        }
+        if (felonsJsx[this.state.index].placeOfBirth != null) {
+            locationImg = <img src={location} class="icon"  alt="location" />   
+        }
+        if (felonsJsx[this.state.index].redFlag != null) {
+            redFlagImg = <img src={red_flag} class="icon"  alt="red flag" />
+        }
+
+        if (felonsJsx[this.state.index].name !== null) {
             return (
                     <div>
                         <div>
                             <div>
-                                <div>{felonsJsx[this.state.index].name}, {felonsJsx[this.state.index].DOB}</div>
+                                <div>{this.state.index}{felonsJsx[this.state.index].name}, {felonsJsx[this.state.index].DOB}</div>
                             </div>
-                            <img src={ethnicity} class="icon"  alt="ethnicity" />
-                            <img src={person} class="icon"  alt="person" />
-                            <img src={location} class="icon"  alt="location" />
-                            <img src={occupation} class="icon"  alt="occupation" />
-                            <img src={red_flag} class="icon"  alt="red flag" />
+                            <div>
+                                {ethnicityImg} 
+                                {felonsJsx[this.state.index].nationality} | {felonsJsx[this.state.index].race}                      
+                            </div>
+                            <div>
+                                {locationImg}   
+                                Originally from {felonsJsx[this.state.index].placeOfBirth}                                        
+                            </div>
+                            <div>
+                                {occupationImg}
+                                {felonsJsx[this.state.index].occupation}
+                            </div>
+                            <div>
+                                {redFlagImg}
+                                {redFlagLower}
+                            </div>
+                            <div>
+                                {felonsJsx[this.state.index].hair} | {felonsJsx[this.state.index].eyes}                           
+                            </div>
                             <button onClick={this.next}>next</button>
                         </div>
                     </div>
